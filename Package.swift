1538fc5c5de54268d852afa4649a97bb464245d1
Andrew Marmion <andrew.marmion@gmail.com>
2020-06-14 16:19:52 +0100

    Add changes to Package.swift

--- Package.swift
+++ Package.swift
@@ -5,10 +5,12 @@ import PackageDescription
 
 let package = Package(
     name: "AMCircleMenu",
+    platforms: [.iOS(.v13)],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
             name: "AMCircleMenu",
+            type: .dynamic,
             targets: ["AMCircleMenu"]),
     ],
     dependencies: [
